af63d2fc311ca638b3cda19c5071f5ef3f10dd36
Abhilash30 <abhilashpawar14@gmail.com>
2026-02-04 00:25:02 +0530

    Camera + tilemapsystem with collisions + refined player movement - jumping feels better now

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,19 +1,26 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
+const SPEED = 150.0
+const JUMP_VELOCITY = -250.0
+const Jump_multiplier = 0.05;
+@onready var sprite = $Sprite2D
+var stretch_amount := 0.40
+var squash_amount := 0.2
+var scale_speed := 10.0
 func _physics_process(delta: float) -> void:
+	var target_scale = Vector2.ONE  
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		if velocity.y<0:
+			target_scale = Vector2(1.0 - stretch_amount, 1.0 + stretch_amount)  
 
-	# Handle jump.
+	# Handle jump . 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+		velocity.y = JUMP_VELOCITY 
+	if Input.is_action_just_released("ui_accept") and velocity.y < 0: 
+		velocity.y *= Jump_multiplier
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
@@ -21,5 +28,6 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
+	sprite.scale = sprite.scale.lerp(target_scale, scale_speed * delta)

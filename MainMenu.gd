d7fb318977b4ac065343c989d454d7dd48eb3efa
Abhilash30 <abhilashpawar14@gmail.com>
2026-02-05 23:56:44 +0530

    multiplayer added

---
+++ MainMenu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+@onready var room_list := $VBoxContainer/RoomList
+@onready var host_button := $VBoxContainer/Host
+
+
+
+
+func _on_host_button_down() -> void:
+	HighLevelNetworkHandler.start_server()
+	visible = false
+
+
+func _on_join_button_down() -> void:
+	HighLevelNetworkHandler.start_client()
+	visible = false
+	pass # Replace with function body.
--- character_body_2d_client.gd
+++ character_body_2d_client.gd
@@ -14,8 +14,7 @@ var scale_speed := 10.0
 
 
 
-func _enter_tree() -> void:
-	set_multiplayer_authority(name.to_int())
+
 
 func _physics_process(delta: float) -> void:
 	if !is_multiplayer_authority(): return
